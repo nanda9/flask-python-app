ad84dd806a7e35b0f8354d4656aeb13e9ec92f29
nanda9 <nandakumar.jraju@gmail.com>
2026-06-23 21:50:49 -0500

    Html removed

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return """
-	<h1>Hello Docker! </h1>
-	<h2>This is my First local host application deployed on Docker container</h2>
-	<h3>Designed by Nandakumar Jyothi, DevOps Engineer</h3>
-	<h3>CI/CD Docker Pipeline Running!</h3>"""
+	CI/CD Docker Pipeline Running!
+    Hello Docker!.. 
+	This is my First local host application deployed on Docker container.
+	Designed by Nandakumar Jyothi, DevOps Engineer.
+	"""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
