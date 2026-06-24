70162a554a8c5746d1251da8386fc3c9cc69f7b8
nanda9 <39308867+nanda9@users.noreply.github.com>
2026-06-23 21:25:30 -0500

    Refactor home route and update run configuration

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
 
-port = int(os.getenv("PORT", 5000))
-
 @app.route("/")
 def home():
-	return """
+    return """
 	<h1>Hello Docker! </h1>
-	<h2>This is my First local host deployed Docker container app</h2>
-	<h3>Designed by Nandakumar Jyothi DevOps Engineer</h3>
-	"""
-
+	<h2>This is my First local host application deployed on Docker container</h2>
+	<h3>Designed by Nandakumar Jyothi, DevOps Engineer</h3>
+	<h3>CI/CD Docker Pipeline Running!</h3>"""
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=port)
-
-
+    app.run(host="0.0.0.0", port=5000)
