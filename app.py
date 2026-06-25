4d4abfc2fb34f52ded597810d8a4f53914a5906f
nanda9 <nandakumar.jraju@gmail.com>
2026-06-25 00:20:51 -0500

    with images

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>End-to-End DevOps Project: CI/CD with Docker, Kubernetes & ArgoCD</h1>
-    <h2>Automated Deployment Triggered by Git Push</h2>
-    <h3>Designed by Nandakumar Jyothi | DevOps Engineer</h3>
-    """
-    
+    return render_template("index.html")
+
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
