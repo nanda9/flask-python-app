5c57a6567ec3c2f9c5362265b96919eba0dead47
nanda9 <nandakumar.jraju@gmail.com>
2026-06-24 23:29:17 -0500

    ArgoCD

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return """
-    <h1> Hello from Kubernetes, Hello from Docker!, Hello from ArgoCD!  CI/CD Docker Pipeline Running! </h1>
+    <h1> CI/CD Docker Pipeline Running! </h1>
     <h2> This is my First local host application deployed on Docker minikube cluster. </h2>
     <h3> Designed by Nandakumar Jyothi, DevOps Engineer. </h3>
     """
