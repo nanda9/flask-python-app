52f24f3e8aadc9ddf1aee3ed0d3336cafbcdf12c
nanda9 <nandakumar.jraju@gmail.com>
2026-06-24 22:27:44 -0500

    Add Kubernetes and GitOps files

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return """
-    <h1>Hello from Render + Docker!  CI/CD Docker Pipeline Running!</h1>
-    <h2>This is my First local host application deployed on Render + Docker container.</h2>
-    <h3>Designed by Nandakumar Jyothi, DevOps Engineer.</h3>
+    <h1> Hello from Kubernetes v4, Hello from Render + Docker!  CI/CD Docker Pipeline Running! </h1>
+    <h2> This is my First local host application deployed on Render + Docker container. </h2>
+    <h3> Designed by Nandakumar Jyothi, DevOps Engineer. </h3>
     """
-
+    
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
