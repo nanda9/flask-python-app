508099362174c74fd11aa12d558d2ef3bd0eb10e
nanda9 <nandakumar.jraju@gmail.com>
2026-06-29 22:18:00 -0500

    Revert "html updated"
    This reverts commit 718b53cba0e3d7cb0a2debbe7c7017042e8a9031.

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# ---------------- HOME PAGE ----------------
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
-# ---------------- METRICS API (USED BY DASHBOARD) ----------------
-@app.route("/metrics")
-def metrics():
-
-    return jsonify({
-        "pods": 3,
-        "cpu": 45,
-        "memory": 62,
-        "commit": "a1b2c3d4"
-    })
-
-
-# ---------------- RUN APP ----------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
