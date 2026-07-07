bffb74d409da1431b06edd3688b1cc2adc0f25d0
nanda9 <nandakumar.jraju@gmail.com>
2026-07-07 08:07:29 -0500

    Add Kubernetes manifests and ArgoCD configuration

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy",
+        "application": "flask-python-app"
+    })
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
