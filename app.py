1dc244ed74d1e7f8d4c31a28d92ace9743f7814f
nanda9 <nandakumar.jraju@gmail.com>
2026-07-10 17:38:38 -0500

    added Configmap

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-    return render_template("index.html")
-
+    return render_template(
+        "index.html",
+        app_name=os.getenv("APP_NAME"),
+        app_env=os.getenv("APP_ENV"),
+        company=os.getenv("COMPANY")
+    )
 
 @app.route("/health")
 def health():
-    return jsonify({
-        "status": "healthy",
-        "application": "flask-python-app"
-    })
-
+    return {"status": "healthy"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
